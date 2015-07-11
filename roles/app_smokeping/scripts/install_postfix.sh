c0386b2c7c13b65a728fb70ac3aa551b324cb60d
Christopher Hicks <chicks.net@gmail.com>
2015-07-11 14:32:07 +0000

    skip writing debug file :busts_in_silhouette:

--- roles/app_smokeping/scripts/install_postfix.sh
+++ roles/app_smokeping/scripts/install_postfix.sh
@@ -4,7 +4,7 @@
 
 REGION=`ec2metadata | grep -a availability-zone | awk '{print $2}'`
 HOSTNAME=`hostname`.$REGION.prfct.co
-echo $HOSTNAME >> /tmp/postfix.hostname
+#echo $HOSTNAME >> /tmp/postfix.hostname
 debconf-set-selections <<< "postfix postfix/mailname string $HOSTNAME"
 debconf-set-selections <<< "postfix postfix/main_mailer_type string 'Internet Site'"
 apt-get install -y postfix
